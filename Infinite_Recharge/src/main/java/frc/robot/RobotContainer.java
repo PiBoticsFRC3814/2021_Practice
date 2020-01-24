842e188e2572d41b4c458248770671976d681942
Logan Lewton <lewtonl@hotmail.com>
2020-01-23 19:47:46 -0500

    Removed orange lines-xavier

--- Infinite_Recharge/src/main/java/frc/robot/RobotContainer.java
+++ Infinite_Recharge/src/main/java/frc/robot/RobotContainer.java
@@ -9,7 +9,6 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.commands.DriveLimelight;
 import frc.robot.commands.PiboticsDrive;
--- Infinite_Recharge/src/main/java/frc/robot/subsystems/Limelight.java
+++ Infinite_Recharge/src/main/java/frc/robot/subsystems/Limelight.java
@@ -8,7 +8,6 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
