f2a73b787feae012f0687aa9505b0128e369cb54
Oldgoer <jacob.newcomer@gmail.com>
2020-10-23 14:23:29 -0400

    Update RobotContainer.java

--- Infinite_Recharge/src/main/java/frc/robot/RobotContainer.java
+++ Infinite_Recharge/src/main/java/frc/robot/RobotContainer.java
@@ -7,13 +7,11 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.*;
 import frc.robot.subsystems.*;
 import frc.robot.Constants;
-import frc.robot.subsystems.Limelight;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -30,21 +28,12 @@ import edu.wpi.first.wpilibj.Joystick;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  ADXRS450_Gyro gyro = new ADXRS450_Gyro();
-
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
   private final DriveTrain m_piboticsdrive = new DriveTrain();
-  private final Shooter m_shooter = new Shooter();
-  private final IntakeMaintain m_IntakeMaintain = new IntakeMaintain();
-  private final Limelight m_LimeLight = new Limelight();
-  private final ClimbMotor m_Climb = new ClimbMotor();
-  private final BalanceMotor m_Balance = new BalanceMotor();
-  private final ControlPanel m_ControlPanel = new ControlPanel();
 
   private final Joystick driverStick = new Joystick(Constants.oi_Driver);
-  private final Joystick buttonStick = new Joystick(Constants.oi_Operator);
 
   private final CommandBase m_autonomousCommand = new Autonomous1(m_piboticsdrive,m_LimeLight,m_shooter,m_IntakeMaintain, gyro);
 
@@ -52,17 +41,12 @@ public class RobotContainer {
    * The container for the robot.  Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() {
-
-    m_piboticsdrive.setDefaultCommand(new PiboticsDrive(() -> driverStick.getY(), () -> driverStick.getZ(), m_piboticsdrive));
-    m_LimeLight.setDefaultCommand(new GetLimelight(m_LimeLight, gyro));
-    m_ControlPanel.setDefaultCommand(new GrabColorData(m_ControlPanel));
-    
+    //below are the default comands that we create these will run by default if nothing else is trying to use the required subsystem thing that need to run all the time 
+    //should be default commands like the drive train...
+    m_piboticsdrive.setDefaultCommand(new PiboticsDrive(() -> driverStick.getY(), () -> driverStick.getZ(), m_piboticsdrive));   
 
     // Configure the button bindings
-    configureButtonBindings();
-
-
-  
+    configureButtonBindings(); //this runs the button config below...  
     
   }
 
@@ -73,70 +57,19 @@ public class RobotContainer {
    * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-    //joystick buttons
-    final JoystickButton Intake = new JoystickButton(driverStick, 7);
-    final JoystickButton shooter = new JoystickButton(driverStick, 2);
-    final JoystickButton LimelightMove =  new JoystickButton(driverStick, 10);
-    final JoystickButton autoIntake = new JoystickButton(driverStick, 6);
-    final JoystickButton Position = new JoystickButton(driverStick, 11);
-    final JoystickButton Rotation = new JoystickButton(driverStick, 12);
-    //final JoystickButton gateTurn = new JoystickButton(driverStick, 9);
-    final JoystickButton FarLimelight = new JoystickButton(driverStick, 9);
+    //joystick buttons that we define
+    final JoystickButton PistonIn = new JoystickButton(driverStick, 5);
+    final JoystickButton PistonOut = new JoystickButton(driverStick, 6);
 
+ 
+    //below is the code thaat make the button do stuff
 
-    //fightstick buttons
-    final JoystickButton climbUp = new JoystickButton(buttonStick, 3);
-    final JoystickButton climbDown = new JoystickButton(buttonStick, 1);
-    final JoystickButton balanceLeft = new JoystickButton(buttonStick, 9);
-    final JoystickButton balanceRight = new JoystickButton(buttonStick, 10);
-    final JoystickButton ToggleLight = new JoystickButton(buttonStick, 4);
-    final JoystickButton Outtake = new JoystickButton(buttonStick, 6);
-
-
-
+    PistonIn.whenPressed(new COMMAND());
+    PistonIn.whenReleased(new COMMAND2());
     
-    
+    PistonOut.whenPressed(new COMMAND3());
+    PistonOut.whenReleased(new COMMAND2());
 
-    shooter.whenPressed(new Shoot(m_shooter));
-    shooter.whenReleased(new StopShoot(m_shooter,m_LimeLight));
-
-    climbUp.whenPressed(new ClimbUp(m_Climb));
-    climbUp.whenReleased(new ClimbStop(m_Climb));
-    climbDown.whenPressed(new ClimbDown(m_Climb));
-    climbDown.whenReleased(new ClimbStop(m_Climb));
-
-    balanceLeft.whenPressed(new BalanceLeft(m_Balance));
-    balanceLeft.whenReleased(new StopBalance(m_Balance));
-    balanceRight.whenPressed(new BalanceRight(m_Balance));
-    balanceRight.whenReleased(new StopBalance(m_Balance));
-
-    //gateTurn.whenPressed(new GateTurn(m_Block));
-    //gateTurn.whenReleased(new GateReturn(m_Block));
-
-    FarLimelight.whenPressed(new FarLimelight(m_piboticsdrive,m_LimeLight, gyro));
-    FarLimelight.whenReleased(new GetLimelight(m_LimeLight, gyro));
-
-
-    Intake.whenPressed(new IntakeOn(m_IntakeMaintain));
-    Intake.whenReleased(new IntakeOff(m_IntakeMaintain));
-
-    Outtake.whenPressed(new IntakeReverse(m_IntakeMaintain));
-    Outtake.whenReleased(new IntakeOff(m_IntakeMaintain));
-    
-
-    LimelightMove.whenPressed(new DriveLimelight(m_piboticsdrive,m_LimeLight, gyro));
-    LimelightMove.whenReleased(new GetLimelight(m_LimeLight, gyro));
-
-    autoIntake.whenPressed(new NewIntakeOn(m_IntakeMaintain));
-    autoIntake.whenReleased(new NewIntakeOff(m_IntakeMaintain));
-
-    Position.whenPressed(new PositionControl(m_ControlPanel));
-    Position.whenReleased(new StopControlPanel(m_ControlPanel));
-    Rotation.whenPressed(new RotationControl(m_ControlPanel));
-    Rotation.whenReleased(new StopControlPanel(m_ControlPanel));
-
-    ToggleLight.whenPressed(new ToggleLimelight(m_LimeLight));
-    ToggleLight.whenReleased(new GetLimelight(m_LimeLight, gyro));
   }
 
 
