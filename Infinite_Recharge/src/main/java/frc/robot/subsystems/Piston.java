6a143ce5b1aaf3f5e8a190bea5b5fbbb318f9993
Oldgoer <jacob.newcomer@gmail.com>
2020-10-23 15:00:43 -0400

    Update Piston.java

--- Infinite_Recharge/src/main/java/frc/robot/subsystems/Piston.java
+++ Infinite_Recharge/src/main/java/frc/robot/subsystems/Piston.java
@@ -8,6 +8,7 @@
 package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import frc.robot.Constants;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
